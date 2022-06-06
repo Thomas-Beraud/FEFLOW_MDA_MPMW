ce783197b53241843bc3bfd322b6cda3df96aa41
Thomas Béraud <thomas.beraud@protonmail.com>
2022-06-06 07:18:27 +0000

    Rename Documentation folder to docs/

--- Functions/create_documentation.bat
+++ Functions/create_documentation.bat
@@ -2,4 +2,4 @@
 
 call E:\Anaconda/Scripts/activate.bat E:\Anaconda
 
-pdoc -o D:\Dropbox\Github_FEFLOW_MDA_MPMW\Documentation D:\Dropbox\Github_FEFLOW_MDA_MPMW\Functions\
+pdoc -o D:\Dropbox\Github_FEFLOW_MDA_MPMW\Docs D:\Dropbox\Github_FEFLOW_MDA_MPMW\Functions\
